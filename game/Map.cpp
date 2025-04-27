70b8517d1950fa8789a03aa5731e85ed1a83c32a
MarcelHB <marscel@googlemail.com>
2025-04-27 19:55:23 +0200

    Map: reducing polygon count

--- game/Map.cpp
+++ game/Map.cpp
@@ -29,53 +29,62 @@ const std::vector<uint32_t>& Map::getVertexIndices() const {
 void Map::tesselateHeightMap() {
   TRACY(ZoneScoped);
 
-  /*
-   0 x0/y0-----x1/y0 1
-       | \  1 / |
-       |  \  /  |
-       | 2 \/ 4 |
-       |   /\   |
-       |  /  \  |
-       | / 3  \ |
-   2 x0/y1-----x1/y1 3
+  std::vector<bool> isFour {};
+  isFour.resize(size.x * size.y, false);
 
-     with each triangle indexed:
+  size_t numTwo = 0;
+  size_t numFour = 0;
 
-         1-----2
-          \   /
-           \ /
-            0
-   */
-
-  auto numUniqueVertices = size.x * size.y + (size.x + 1) * (size.y + 1);
-  // centers: size.x * size.y
-  // top-lefts: size.x * size.y
-  // bottoms: size.x + 1
-  // rights: size.y
-  verticesAndNormals.resize(numUniqueVertices * 2);
-
-  auto numVertices = size.x * size.y * 12;
-  vertexIndices.resize(numVertices);
-
-  // Setup of index slots
+  // Scan where we want 2 or 4 triangles (not planar)
   for (size_t y = 0; y < size.y; ++y) {
     for (size_t x = 0; x < size.x; ++x) {
-      // center
       auto h1 = getHeight(x, y, 0);
       auto h2 = getHeight(x, y, 1);
       auto h3 = getHeight(x, y, 2);
       auto h4 = getHeight(x, y, 3);
 
-      auto centerHeight = (h1 + h2 + h3 + h4) / 4.0f;
-      auto centerIdx = y * size.x + x;
+      if (h1 == h2 && h2 == h3 && h3 == h4) {
+        numTwo++;
+      } else {
+        numFour++;
+        isFour[y * size.x + x] = true;
+      }
+    }
+  }
 
-      auto& centerPos = verticesAndNormals[centerIdx].position;
-      centerPos.x = x + 0.5f;
-      centerPos.y = centerHeight;
-      centerPos.z = y + 0.5f;
+  // four centers: n4
+  // top-lefts: size.x * size.y
+  // bottoms: size.x + 1
+  // rights: size.y
+  auto numUniqueVertices = numFour + size.x * size.y + size.x + 1 + size.y + 1;
+  verticesAndNormals.resize(numUniqueVertices);
+
+  auto numVertices = numFour * 12 + numTwo * 6;
+  vertexIndices.resize(numVertices);
+
+  size_t i4 = 0;
+  // Setup of index slots
+  for (size_t y = 0; y < size.y; ++y) {
+    for (size_t x = 0; x < size.x; ++x) {
+      auto h1 = getHeight(x, y, 0);
+
+      if (isFour[y * size.x + x]) {
+        // center
+        auto h2 = getHeight(x, y, 1);
+        auto h3 = getHeight(x, y, 2);
+        auto h4 = getHeight(x, y, 3);
+
+        auto centerHeight = (h1 + h2 + h3 + h4) / 4.0f;
+        auto centerIdx = i4++;
+
+        auto& centerPos = verticesAndNormals[centerIdx].position;
+        centerPos.x = x + 0.5f;
+        centerPos.y = centerHeight;
+        centerPos.z = y + 0.5f;
+      }
 
       // top left
-      auto topLeftIdx = size.x * size.y + y * size.x + x;
+      auto topLeftIdx = numFour + y * size.x + x;
       auto& topLeftPos = verticesAndNormals[topLeftIdx].position;
       topLeftPos.x = x;
       topLeftPos.y = h1;
@@ -85,7 +94,7 @@ void Map::tesselateHeightMap() {
 
   // bottom
   for (size_t x = 0; x <= size.x; ++x) {
-    auto bottomIdx = 2 * size.x * size.y + x;
+    auto bottomIdx = numFour + size.x * size.y + x;
     auto height = getHeight(x, size.y - 1, 2);
 
     auto& topLeftPos = verticesAndNormals[bottomIdx].position;
@@ -96,7 +105,7 @@ void Map::tesselateHeightMap() {
 
   // right
   for (size_t y = 0; y < size.y; ++y) {
-    auto rightIdx = 2 * size.x * size.y + size.x + 1 + y;
+    auto rightIdx = numFour + size.x * size.y + size.x + 1 + y;
     auto height = getHeight(size.x - 1, y, 1);
 
     auto& rightPos = verticesAndNormals[rightIdx].position;
@@ -105,64 +114,106 @@ void Map::tesselateHeightMap() {
     rightPos.z = y;
   }
 
-  // Connecting the dots (can be merged into the top loop when tested)
+  /*
+     four triangles:
+
+   0 x0/y0-----x1/y0 1
+       | \  1 / |
+       |  \  /  |
+       | 2 \/ 4 |
+       |   /\   |
+       |  /  \  |
+       | / 3  \ |
+   2 x0/y1-----x1/y1 3
+
+   with each triangle indexed:
+
+         1-----2
+          \   /
+           \ /
+            0
+
+  two triangles: top-right + bottom-left
+
+   */
+
+  size_t baseIdx = 0;
+  i4 = 0;
+
   for (size_t y = 0; y < size.y; ++y) {
     for (size_t x = 0; x < size.x; ++x) {
-      auto baseIdx = y * size.x * 12 + x * 12;
+      if (isFour[y * size.x + x]) {
+        // top triangle
+        vertexIndices[baseIdx] = i4;
+        vertexIndices[baseIdx + 1] = numFour + y * size.x + x;
 
-      // top triangle
-      vertexIndices[baseIdx] = y * size.x + x;
-      vertexIndices[baseIdx + 1] = size.x * size.y + y * size.x + x;
-
-      if (x == size.x - 1) {
-        vertexIndices[baseIdx + 2] = 2 * size.x * size.y + size.x + 1 + y;
-      } else {
-        vertexIndices[baseIdx + 2] = size.x * size.y + y * size.x + (x + 1);
-      }
-
-      // left triangle
-      vertexIndices[baseIdx + 3] = y * size.x + x;
-      if (y == size.y - 1) {
-        vertexIndices[baseIdx + 4] = 2 * size.x * size.y + x;
-      } else {
-        vertexIndices[baseIdx + 4] = size.x * size.y + (y + 1) * size.x + x;
-      }
-      vertexIndices[baseIdx + 5] = size.x * size.y + y * size.x + x;
-
-      // bottom triangle
-      vertexIndices[baseIdx + 6] = y * size.x + x;
-      if (y == size.y - 1) {
-        vertexIndices[baseIdx + 7] = 2 * size.x * size.y + x + 1;
-      } else {
         if (x == size.x - 1) {
-          vertexIndices[baseIdx + 7] = 2 * size.x * size.y + size.x + 1 + y + 1;
+          vertexIndices[baseIdx + 2] = numFour + size.x * size.y + size.x + 1 + y;
         } else {
-          vertexIndices[baseIdx + 7] = size.x * size.y + (y + 1) * size.x + x + 1;
+          vertexIndices[baseIdx + 2] = numFour + y * size.x + (x + 1);
         }
-      }
 
-      if (y == size.y - 1) {
-        vertexIndices[baseIdx + 8] = 2 * size.x * size.y + x;
-      } else {
-        vertexIndices[baseIdx + 8] = size.x * size.y + (y + 1) * size.x + x;
-      }
+        // left triangle
+        vertexIndices[baseIdx + 3] = i4;
+        if (y == size.y - 1) {
+          vertexIndices[baseIdx + 4] = numFour + size.x * size.y + x;
+        } else {
+          vertexIndices[baseIdx + 4] = numFour + (y + 1) * size.x + x;
+        }
+        vertexIndices[baseIdx + 5] = vertexIndices[baseIdx + 2];
 
-      // right triangle
-      vertexIndices[baseIdx + 9] = y * size.x + x;
-      if (x == size.x - 1) {
-        vertexIndices[baseIdx + 10] = 2 * size.x * size.y + size.x + 1 + y;
-      } else {
-        vertexIndices[baseIdx + 10] = size.x * size.y + y * size.x + x + 1;
-      }
+        // bottom triangle
+        vertexIndices[baseIdx + 6] = i4;
+        if (y == size.y - 1) {
+          vertexIndices[baseIdx + 7] = numFour + size.x * size.y + x + 1;
+        } else {
+          if (x == size.x - 1) {
+            vertexIndices[baseIdx + 7] = numFour + size.x * size.y + size.x + 1 + y + 1;
+          } else {
+            vertexIndices[baseIdx + 7] = numFour + (y + 1) * size.x + x + 1;
+          }
+        }
 
-      if (y == size.y - 1) {
-        vertexIndices[baseIdx + 11] = 2 * size.x * size.y + x + 1;
+        vertexIndices[baseIdx + 8] = vertexIndices[baseIdx + 4];
+
+        // right triangle
+        vertexIndices[baseIdx + 9] = i4;
+        vertexIndices[baseIdx + 10] = vertexIndices[baseIdx + 2];
+        vertexIndices[baseIdx + 11] = vertexIndices[baseIdx + 7];
+
+        i4++;
+        baseIdx += 12;
       } else {
+        // top right triangle
+        // top right -> bottom right -> top left
         if (x == size.x - 1) {
-          vertexIndices[baseIdx + 11] = 2 * size.x * size.y + size.x + 1 + y + 1;
+          vertexIndices[baseIdx] = numFour + size.x * size.y + size.x + 1 + y;
         } else {
-          vertexIndices[baseIdx + 11] = size.x * size.y + (y + 1) * size.x + x + 1;
+          vertexIndices[baseIdx] = numFour + y * size.y + x + 1;
         }
+
+        if (y == size.y - 1) {
+          vertexIndices[baseIdx + 1] = numFour + size.x * size.y + x + 1;
+        } else {
+          if (x == size.x - 1) {
+            vertexIndices[baseIdx + 1] = numFour + size.x * size.y + size.x + 1 + y + 1;
+          } else {
+            vertexIndices[baseIdx + 1] = numFour + (y + 1) * size.y + x + 1;
+          }
+        }
+
+        vertexIndices[baseIdx + 2] = numFour + y * size.y + x;
+
+        if (y == size.y - 1) {
+          vertexIndices[baseIdx + 3] = numFour + size.x * size.x + y;
+        } else {
+          vertexIndices[baseIdx + 3] = numFour + (y + 1) * size.y + x;
+        }
+
+        vertexIndices[baseIdx + 4] = vertexIndices[baseIdx + 2];
+        vertexIndices[baseIdx + 5] = vertexIndices[baseIdx + 1];
+
+        baseIdx += 6;
       }
     }
   }
