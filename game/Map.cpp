695dabed218077c2f0c3605847b78f27a2f84a3a
MarcelHB <marscel@googlemail.com>
2025-05-03 02:33:05 +0200

    Map: changes to tessellation

--- game/Map.cpp
+++ game/Map.cpp
@@ -55,204 +55,44 @@ void Map::tesselateHeightMap(
 ) {
   TRACY(ZoneScoped);
 
-  std::vector<bool> isFour {};
-  isFour.resize(size.x * size.y, false);
-
-  size_t numTwo = 0;
-  size_t numFour = 0;
-
-  // Scan where we want 2 or 4 triangles (not planar)
-  for (size_t y = 0; y < size.y; ++y) {
-    for (size_t x = 0; x < size.x; ++x) {
-      auto h1 = getHeight(x, y, 0); // tl
-      auto h2 = getHeight(x, y, 1); // tr
-      auto h3 = getHeight(x, y, 2); // bl
-      auto h4 = getHeight(x, y, 3); // br
-
-      if ((h1 == h2 && h3 == h4) || (h1 == h3 && h2 == h4)) {
-        numTwo++;
-      } else {
-        numFour++;
-        isFour[y * size.x + x] = true;
-      }
-    }
-  }
-
-  // four centers: n4
-  // top-lefts: size.x * size.y
-  // bottoms: size.x + 1
-  // rights: size.y
-  auto numUniqueVertices = numFour + size.x * size.y + size.x + 1 + size.y + 1;
+  auto numUniqueVertices = size.x * size.y * 4;
   verticesAndNormals.resize(numUniqueVertices);
 
-  auto numVertices = numFour * 12 + numTwo * 6;
+  auto numVertices = size.x * size.y * 6;
   vertexIndices.resize(numVertices);
 
-  size_t i4 = 0;
-  // Setup of index slots
   for (size_t y = 0; y < size.y; ++y) {
     for (size_t x = 0; x < size.x; ++x) {
-      auto h1 = getHeight(x, y, 0);
+      size_t baseIdx = (y * size.x + x) * 4;
 
-      if (isFour[y * size.x + x]) {
-        // center
-        auto h2 = getHeight(x, y, 1);
-        auto h3 = getHeight(x, y, 2);
-        auto h4 = getHeight(x, y, 3);
+      for (uint8_t i = 0; i < 4; ++i) {
+        auto height = getHeight(x, y, i);
+        auto& vertex = verticesAndNormals[baseIdx + i];
+        auto& position = vertex.position;
 
-        auto centerHeight = (h1 + h2 + h3 + h4) / 4.0f;
-        auto centerIdx = i4++;
+        float xOffset = 0.0f;
+        float yOffset = 0.0f;
+        if (i == 1 || i == 3) {
+          xOffset = 1.0f;
+        }
+        if (i == 2 || i == 3) {
+          yOffset = 1.0f;
+        }
 
-        auto& centerVertex = verticesAndNormals[centerIdx];
-        auto& centerPos = centerVertex.position;
-        centerPos.x = x + 0.5f;
-        centerPos.y = centerHeight;
-        centerPos.z = y + 0.5f;
+        position.x = x + xOffset;
+        position.y = height;
+        position.z = y + yOffset;
 
-        setVertexUV(centerVertex, textureClasses, tileIndex[y * size.x + x], 0.5f, 0.5f);
+        setVertexUV(vertex, textureClasses, tileIndex[y * size.x + x], xOffset, yOffset);
       }
 
-      // top left
-      auto topLeftIdx = numFour + y * size.x + x;
-      auto& topLeftVertex = verticesAndNormals[topLeftIdx];
-      auto& topLeftPos = topLeftVertex.position;
-      topLeftPos.x = x;
-      topLeftPos.y = h1;
-      topLeftPos.z = y;
-
-      setVertexUV(topLeftVertex, textureClasses, tileIndex[y * size.x + x]);
-    }
-  }
-
-  // bottom
-  for (size_t x = 0; x <= size.x; ++x) {
-    auto bottomIdx = numFour + size.x * size.y + x;
-    auto height = getHeight(std::min(x, size.x - size_t{1}), size.y - 1, 2);
-
-    auto& bottomVertex = verticesAndNormals[bottomIdx];
-    auto& bottomPos = bottomVertex.position;
-    bottomPos.x = x;
-    bottomPos.y = height;
-    bottomPos.z = size.y;
-
-    setVertexUV(bottomVertex, textureClasses, tileIndex[(size.y - 1) * size.x + std::min(x, size.x - size_t{1})]);
-  }
-
-  // right
-  for (size_t y = 0; y < size.y; ++y) {
-    auto rightIdx = numFour + size.x * size.y + size.x + 1 + y;
-    auto height = getHeight(size.x - 1, y, 1);
-
-    auto& rightVertex = verticesAndNormals[rightIdx];
-    auto& rightPos = rightVertex.position;
-    rightPos.x = size.x;
-    rightPos.y = height;
-    rightPos.z = y;
-
-    setVertexUV(rightVertex, textureClasses, tileIndex[y * size.x + (size.x - 1)]);
-  }
-
-  /*
-     four triangles:
-
-   0 x0/y0-----x1/y0 1
-       | \  1 / |
-       |  \  /  |
-       | 2 \/ 4 |
-       |   /\   |
-       |  /  \  |
-       | / 3  \ |
-   2 x0/y1-----x1/y1 3
-
-   with each triangle indexed:
-
-         1-----2
-          \   /
-           \ /
-            0
-
-  two triangles: top-right + bottom-left
-
-   */
-
-  size_t baseIdx = 0;
-  i4 = 0;
-
-  for (size_t y = 0; y < size.y; ++y) {
-    for (size_t x = 0; x < size.x; ++x) {
-      if (isFour[y * size.x + x]) {
-        // top triangle
-        vertexIndices[baseIdx] = i4;
-        vertexIndices[baseIdx + 1] = numFour + y * size.x + x;
-
-        if (x == size.x - 1) {
-          vertexIndices[baseIdx + 2] = numFour + size.x * size.y + size.x + 1 + y;
-        } else {
-          vertexIndices[baseIdx + 2] = numFour + y * size.x + (x + 1);
-        }
-
-        // left triangle
-        vertexIndices[baseIdx + 3] = i4;
-        if (y == size.y - 1) {
-          vertexIndices[baseIdx + 4] = numFour + size.x * size.y + x;
-        } else {
-          vertexIndices[baseIdx + 4] = numFour + (y + 1) * size.x + x;
-        }
-        vertexIndices[baseIdx + 5] = vertexIndices[baseIdx + 1];
-
-        // bottom triangle
-        vertexIndices[baseIdx + 6] = i4;
-        if (y == size.y - 1) {
-          vertexIndices[baseIdx + 7] = numFour + size.x * size.y + x + 1;
-        } else {
-          if (x == size.x - 1) {
-            vertexIndices[baseIdx + 7] = numFour + size.x * size.y + size.x + 1 + y + 1;
-          } else {
-            vertexIndices[baseIdx + 7] = numFour + (y + 1) * size.x + x + 1;
-          }
-        }
-
-        vertexIndices[baseIdx + 8] = vertexIndices[baseIdx + 4];
-
-        // right triangle
-        vertexIndices[baseIdx + 9] = i4;
-        vertexIndices[baseIdx + 10] = vertexIndices[baseIdx + 2];
-        vertexIndices[baseIdx + 11] = vertexIndices[baseIdx + 7];
-
-        i4++;
-        baseIdx += 12;
-      } else {
-        // top right triangle
-        // top right -> bottom right -> top left
-        if (x == size.x - 1) {
-          vertexIndices[baseIdx] = numFour + size.x * size.y + size.x + 1 + y;
-        } else {
-          vertexIndices[baseIdx] = numFour + y * size.y + x + 1;
-        }
-
-        if (y == size.y - 1) {
-          vertexIndices[baseIdx + 1] = numFour + size.x * size.y + x + 1;
-        } else {
-          if (x == size.x - 1) {
-            vertexIndices[baseIdx + 1] = numFour + size.x * size.y + size.x + 1 + y + 1;
-          } else {
-            vertexIndices[baseIdx + 1] = numFour + (y + 1) * size.y + x + 1;
-          }
-        }
-
-        vertexIndices[baseIdx + 2] = numFour + y * size.y + x;
-
-        if (y == size.y - 1) {
-          vertexIndices[baseIdx + 3] = numFour + size.x * size.x + y;
-        } else {
-          vertexIndices[baseIdx + 3] = numFour + (y + 1) * size.y + x;
-        }
-
-        vertexIndices[baseIdx + 4] = vertexIndices[baseIdx + 2];
-        vertexIndices[baseIdx + 5] = vertexIndices[baseIdx + 1];
-
-        baseIdx += 6;
-      }
+      size_t vertexIdx = (y * size.x + x) * 6;
+      vertexIndices[vertexIdx] = baseIdx;
+      vertexIndices[vertexIdx + 1] = baseIdx + 1;
+      vertexIndices[vertexIdx + 2] = baseIdx + 2;
+      vertexIndices[vertexIdx + 3] = baseIdx + 1;
+      vertexIndices[vertexIdx + 4] = baseIdx + 3;
+      vertexIndices[vertexIdx + 5] = baseIdx + 2;
     }
   }
 }
@@ -293,8 +133,9 @@ float Map::getHeight(size_t x, size_t y, uint8_t corner) {
       auto h1 = getHeight(x + 1, y);
       auto h2 = getHeight(x, y + 1);
       auto h3 = getHeight(x, y);
+      auto h4 = getHeight(x + 1, y + 1);
 
-      return (h1 + h2 + h3) / 3.0f;
+      return (h1 + h2 + h3 + h4) / 4.0f;
     }
   } // bottom-left
   else if (corner == 2) {
@@ -318,8 +159,9 @@ float Map::getHeight(size_t x, size_t y, uint8_t corner) {
       auto h1 = getHeight(x - 1, y);
       auto h2 = getHeight(x, y + 1);
       auto h3 = getHeight(x, y);
+      auto h4 = getHeight(x - 1, y + 1);
 
-      return (h1 + h2 + h3) / 3.0f;
+      return (h1 + h2 + h3 + h4) / 4.0f;
     }
   } // top-right
   else if (corner == 1) {
@@ -343,8 +185,9 @@ float Map::getHeight(size_t x, size_t y, uint8_t corner) {
       auto h1 = getHeight(x + 1, y);
       auto h2 = getHeight(x, y - 1);
       auto h3 = getHeight(x, y);
+      auto h4 = getHeight(x + 1, y - 1);
 
-      return (h1 + h2 + h3) / 3.0f;
+      return (h1 + h2 + h3 + h4) / 4.0f;
     }
   } // top-left
   else {
@@ -368,8 +211,9 @@ float Map::getHeight(size_t x, size_t y, uint8_t corner) {
       auto h1 = getHeight(x - 1, y);
       auto h2 = getHeight(x, y - 1);
       auto h3 = getHeight(x, y);
+      auto h4 = getHeight(x - 1, y - 1);
 
-      return (h1 + h2 + h3) / 3.0f;
+      return (h1 + h2 + h3 + h4) / 4.0f;
     }
   }
 }
@@ -384,24 +228,24 @@ void Map::setVertexUV(
   size_t textureIndex = 0;
 
   for (; textureIndex < textureClasses.size(); ++textureIndex) {
-    if (textureClasses[textureIndex].firstTile >= vertexIndex) {
+    if (textureClasses[textureIndex].firstTile > vertexIndex) {
       break;
     }
   }
-
-  if (textureIndex >= textureClasses.size()) {
-    textureIndex = textureClasses.size() - 1;
+  if (textureIndex > 0) {
+    textureIndex -= 1;
   }
 
   auto& textureClass = textureClasses[textureIndex];
-  auto textureTileIndex = vertexIndex - textureClass.firstTile;
+  auto textureTileIndex =
+    std::min(uint32_t{vertexIndex}, textureClass.firstTile + textureClass.numTiles - 1) - textureClass.firstTile;
   auto x = textureTileIndex % textureClass.width;
   auto y = textureTileIndex / textureClass.width;
 
   auto unit = 1.0f / textureClass.width;
 
   vertexData.uv.x = x * unit + xOffset * unit;
-  vertexData.uv.y = x * unit + xOffset * unit;
+  vertexData.uv.y = y * unit + yOffset * unit;
   vertexData.textureIdx = textureIndex;
 }
 
