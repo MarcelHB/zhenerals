6b2f43ad840484f1a0d215e6886220a9d3b1a9a6
MarcelHB <marscel@googlemail.com>
2025-09-06 12:15:09 +0200

    MemProfiling: cleanup of unused code

--- game/MemProfiling.cpp
+++ game/MemProfiling.cpp
@@ -5,9 +5,6 @@
 
 #if defined(USE_TRACY) && !defined(NO_TRACY) && defined(USE_TRACY_MEMORY)
 
-void* allocations[4096] = {0};
-std::atomic<std::size_t> j;
-
 void* operator new(size_t count) {
   auto ptr = malloc(count);
   TracyAlloc(ptr, count);
