30bc4e9891cfb35a9081c0f0aab2eb428ad5f47c
MarcelHB <marscel@googlemail.com>
2026-08-14 22:46:28 +0200

    Config: restores VSync default to `false`

--- game/Config.h
+++ game/Config.h
@@ -15,7 +15,7 @@ namespace ZH {
 struct Config {
   Size resolution = {1600, 900};
   bool fullscreen = false;
-  bool vSync = false;
+  bool vSync = true;
   std::optional<uint16_t> refreshRate;
   Vugl::MSAASampling msaaSampling = Vugl::MSAASampling::EIGHT;
   float maxAnisotropy = 4.0f;
